897a92bafc84f95bda3ae42cb97f43e6eca1cf8a
Anant Sinha <anantsinha9811@gmail.com>
2020-07-17 03:32:00 +0530

    Convert state and crime to factors

--- parole2.R
+++ parole2.R
@@ -6,3 +6,8 @@ str(parole)
 nrow(subset(parole, violator ==1))
 summary(parole)
 head(parole)
+
+# convert state and crime to factors
+parole$state <- as.factor(parole$state)
+parole$crime <- as.factor(parole$crime)
+summary(parole$state)
